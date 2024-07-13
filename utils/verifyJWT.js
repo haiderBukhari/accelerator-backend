fabd80704bcbbac1942b79049c2f4efce21001c9
Muhammad Haider Bukhari <apshaiderbukhari786@gmail.com>
2024-07-13 17:07:35 +0500

    auth token issues

--- utils/verifyJWT.js
+++ utils/verifyJWT.js
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken'
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.token;
-
-    if (!token) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
         return res.status(401).json({ message: 'No token provided' });
     }
 
+    const token = authHeader.split(' ')[1];
+    
     jwt.verify(token, process.env.COOKIE_SECRET, (err, decoded) => {
         if (err) {
             return res.status(401).json({ message: 'Invalid token' });
